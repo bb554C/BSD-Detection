67dfbad15916a4ed4a737867fbead311642211f0
bb554C <bairdbuenaventura@gmail.com>
2022-03-10 19:43:29 +0800

    Initial working Training code using pytorch and converted to ONXX. Trained and tested using dogcat

--- Testing Code/test.py
+++
@@ -1,7 +0,0 @@
-# Read the categories
-with open("imagenet_classes.txt", "r") as f:
-    categories = [s.strip() for s in f.readlines()]
-# Show top categories per image
-top5_prob, top5_catid = torch.topk(probabilities, 5)
-for i in range(top5_prob.size(0)):
-    print(categories[top5_catid[i]], top5_prob[i].item())
